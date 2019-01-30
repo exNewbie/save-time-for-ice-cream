67431e6233e4f220369a6df6e35e4e60356a09cc
Trung Ly <trung@plantminer.com.au>
2019-01-30 10:05:16 +1000

    bash/execute-sql-file.sh
    python/mongodb_srv_records.py

--- bash/execute-sql-file.sh
+++ bash/execute-sql-file.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-## Usage: execute-sql-file.sh /etc/MYSQL_CLIENT_CONF sample_db queries.sql
+## Usage: execute-sql-file.sh /etc/MYSQL_CLIENT_CONF sample_db queries.sql no-sql no-col-names
 set -e
 
 MYSQL_CONF=$1;
 DATABASE=$2;
 SQL_FILE=$3;
+NO_SQL=$4;
+NO_COL_NAMES=$5;
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-  echo $line;
-  mysql --defaults-extra-file=${MYSQL_CONF} ${DATABASE} -e "$line";
+  if [ "$NO_SQL" != 'no-sql' ]; then
+    echo $line;
+  fi
+
+  if [ "$NO_COL_NAMES" != 'no-col-names' ]; then
+    NO_COL_NAMES="-N"
+  else
+    NO_COL_NAMES=""
+  fi
+
+  mysql --defaults-extra-file=${MYSQL_CONF} ${DATABASE} ${NO_COL_NAMES} -e "$line";
   echo "=================================================================================================================================================";
 done < "${SQL_FILE}"
