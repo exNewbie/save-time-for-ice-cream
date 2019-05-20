c0a68d3112e661162ef6e70e73091a075413be8c
Trung Ly <trung@plantminer.com.au>
2019-05-20 15:37:28 +1000

    improvement

--- bash/optus.sh
+++ bash/optus.sh
@@ -3,14 +3,20 @@
 MY_IP=$( curl -s ifconfig.io );
 MOBILE_IP_PREFIX=49;
 ARN=$1;
+LOG_FILE="/var/log/optus.log";
 
-message="Current IP: ${MY_IP}"
+echo `date` >> $LOG_FILE;
 
-if [[ $MY_IP == "${MOBILE_IP_PREFIX}.*" ]]; then
+message="Current IP: ${MY_IP}";
+
+if [[ $MY_IP =~ ^${MOBILE_IP_PREFIX}.* ]]; then
   message="On LTE. ${message}";
-  aws sns publish --message "${message}" --topic-arn $ARN
+  aws sns publish --message "${message}" --topic-arn $ARN;
 else
   message="On NBN. ${message}";
 fi
 
+echo $message >> $LOG_FILE;
+echo '===================================================' >> $LOG_FILE;
 echo $message;
+
