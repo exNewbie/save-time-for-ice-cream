8855a0b7cad4f5aadcf7ecfb5390c44c5386d23b
Trung Ly <14102056+exNewbie@users.noreply.github.com>
2026-06-19 15:40:27 +1000

    update update-ls-fw.sh

--- bash/update-ls-fw.sh
+++ bash/update-ls-fw.sh
@@ -3,7 +3,7 @@
 MY_IP=$( curl -s ifconfig.io )
 OFFICE_IP="xxx.xxx.xxx.xxx/32"
 
-PORT_INFOS='[{ "protocol": "TCP", "fromPort": 32, "toPort": 32 }, { "protocol": "TCP", "fromPort": 33, "toPort": 33 }, { "protocol": "UDP", "fromPort": 1194, "toPort": 1194 }, { "protocol": "TCP", "fromPort": 53, "toPort": 53, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "UDP", "fromPort": 53, "toPort": 53, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 1081, "toPort": 1081, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 82, "toPort": 82, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 9091, "toPort": 9091, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }]'
+PORT_INFOS='[{ "protocol": "TCP", "fromPort": 32, "toPort": 32 }, { "protocol": "TCP", "fromPort": 33, "toPort": 33 }, { "protocol": "UDP", "fromPort": 1194, "toPort": 1194 }, { "protocol": "TCP", "fromPort": 53, "toPort": 53, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "UDP", "fromPort": 53, "toPort": 53, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 1081, "toPort": 1081, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 82, "toPort": 82, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 9091, "toPort": 9091, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }, { "protocol": "TCP", "fromPort": 8097, "toPort": 8097, "cidrs": [ "OFFICE_IP", "INSTANCE_PUB_IP", "TEMPLATE_IP" ] }]'
 
 echo "Current IP: ${MY_IP}"
 
